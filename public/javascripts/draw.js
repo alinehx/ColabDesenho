f170258be192d80ce7a36e657b600a995465cfd9
Aline Lavorato <alinehlavorato@gmail.com>
2015-10-06 17:21:15 -0300

    Projeto Pronto

---
+++ public/javascripts/draw.js
@@ -0,0 +1,60 @@
+
+tool.maxDistance = 50;
+
+
+
+function randomCor() {
+  
+  return {
+    vermelho: 0,
+    verde: Math.random(),
+    azul: Math.random(),
+    alfa: ( Math.random() * 0.25 ) + 0.05
+  };
+
+}
+
+
+function onMouseDrag(event) {
+
+  
+  var x = event.middlePoint.x;
+  var y = event.middlePoint.y;
+  
+  // de acordo com a velocidade do mouse aumenta o raio
+  //var raio = event.delta.length / 2;  
+ 
+  var cor = randomCor();
+ 
+  drawCircle( x, y, 10, cor );  
+  
+  emitCircle( x, y, 10, cor );
+
+}
+
+
+function drawCircle( x, y, raio, cor ) {
+  
+  var circle = new Path.Circle( new Point( x, y ), raio);
+  circle.fillColor = new RgbColor( cor.vermelho, cor.verde, cor.azul, cor.alfa );
+  
+  view.draw();
+}
+
+function emitCircle( x, y, raio, cor ) {
+
+  var sessionId = io.socket.sessionid;  
+ 
+  var data = {
+    x: x,
+    y: y,
+    raio: raio,
+    cor: cor
+  };
+ 
+  io.emit('drawCircle', data, sessionId);
+}
+
+io.on( 'drawCircle', function( data ) {  
+  drawCircle( data.x, data.y, data.raio, data.cor );  
+})
---
+++ public/javascripts/socket.js
@@ -0,0 +1,10 @@
+
+io = io.connect('/');
+
+console.log( "socket: browser says ping (1)" )
+io.emit('ping', { some: 'data' } );
+
+
+io.on('pong', function (data) {
+	console.log( 'socket: server said pong (4)', data );
+});
